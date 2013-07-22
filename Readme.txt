c450c0ff03dca91fd794e4a2728a219b1f0b6879
arkapravamajumder <arkapravamajumder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-22 17:48:49 +0000

    commit
    git-svn-id: https://plugins.svn.wordpress.org/backtotop/trunk@744528 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,47 @@
+=== Back To Top ===
+Contributors: Arkaprava Majumder
+Tags: backtotop,scrolltotop, top, smooth scroll to top
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: trunk
+License: GPL or later
+
+This plugin will create a "back to top" or "scroll to top button" for your website with smooth scrolling and 25 images.
+== Description ==
+
+This plugin will create a "back to top" or "scroll to top button" for your website with smooth scrolling and 25 images.
+Demo link:http://www.pilgrimsholidays.com 
+ 
+ 
+Developed By : Arkaprava majumder (http://fb.com/arkaindas)
+
+== Installation ==
+
+1. Download Back To Top.
+2. Upload to your Site,(/wp-content/plugins/)
+3. Activate it.
+
+Now, Go to Setting----> Back To Top Setting----> Choose Any image----> Then save it.
+
+Yeah! You're done!
+
+Uninstalling is as simple as deactivating the plugin.
+
+== Screenshots ==
+
+1. Website or Front End Screenshot
+
+2. Admin panel or back End Screenshot
+
+3. Back To Top Settings
+
+== Frequently Asked Questions ==
+
+None yet, if you have one post it at http://arkapravamajumder.com 
+or, Fb me Arkaprava majumder (http://fb.com/arkaindas)
+or, Tweet me (http://twitter.com/arkaindas)
+
+== Change Log ==
+= Version 1.0 =
+
+
---
+++ backtotop.php
@@ -0,0 +1,51 @@
+<?php
+/*
+  Plugin Name: Back To Top
+  Plugin URI: http://arkapravamajumder.com
+  Author URI: http://www.fb.com/arkaindas
+  Author: Arkaprava majumder
+  Version:1.0
+  Description: This plugin will create a "back to top" or "scroll to top button" for your website with smooth scrolling and 25 images.
+  License: GPL
+  
+ */
+define('PLUGIN_DIR_NAME', 'backtotop');
+
+class backtotop{
+	function __construct(){
+		add_action("wp_enqueue_scripts",array(&$this,'register_javascript'));
+		add_action("wp_enqueue_scripts",array(&$this,'register_css'));
+		add_action("wp_footer",array(&$this,'add_my_div'));
+		add_action("admin_menu",array(&$this,'add_my_menu'));
+		}
+	function add_my_div(){
+		
+	$image_value=get_option("image_name");
+	
+				echo "<div  id=\"top\"><img style=\"max-width:120px;max-height:80px;\" src=\"".plugins_url(PLUGIN_DIR_NAME."/img/".$image_value.".png")."\"></div>";
+			
+	
+		}
+	function register_javascript(){
+		wp_enqueue_script('jquery');
+		wp_register_script('backtotopjs',plugins_url(PLUGIN_DIR_NAME.'/js/backtotopbyarka.js'),'','',true);
+		wp_enqueue_script('backtotopjs');
+		}
+		
+	function register_css(){
+		wp_register_style('backtotopcss',plugins_url(PLUGIN_DIR_NAME.'/css/backtotopbyarka.css'));
+		wp_enqueue_style('backtotopcss');
+		
+		}
+	function add_my_menu(){
+		
+		add_options_page('Back To Top Settings','Back To Top Settings','6','backtotopmenu',array(&$this,'my_menu_content'));
+		}
+	function my_menu_content(){
+		include('inc/backend_body.php');
+		}
+	
+	}
+	new backtotop();
+
+?>
